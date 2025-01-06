161dbb71158ac08911fca5518ab8804465826c84
Axel Calås <axel.calas.semst@gmail.com>
2025-01-06 14:20:15 +0100

    Added rating to Feedback model & migrated new database table

---
+++ HealthCareABApi/Migrations/20250106114136_Adjusted Feedback.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace HealthCareABApi.Migrations
+{
+    /// <inheritdoc />
+    public partial class AdjustedFeedback : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "Rating",
+                table: "Feedback",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Rating",
+                table: "Feedback");
+        }
+    }
+}
