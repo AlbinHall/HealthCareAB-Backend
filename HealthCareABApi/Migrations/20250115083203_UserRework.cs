acfe078688b4cb06f489346d75e50a87857c9c7c
Axel Calås <axel.calas.semst@gmail.com>
2025-01-15 09:42:15 +0100

    New migration file for the new user class.

---
+++ HealthCareABApi/Migrations/20250115083203_UserRework.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace HealthCareABApi.Migrations
+{
+    /// <inheritdoc />
+    public partial class UserRework : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Email",
+                table: "User",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Firstname",
+                table: "User",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Lastname",
+                table: "User",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Email",
+                table: "User");
+
+            migrationBuilder.DropColumn(
+                name: "Firstname",
+                table: "User");
+
+            migrationBuilder.DropColumn(
+                name: "Lastname",
+                table: "User");
+        }
+    }
+}
