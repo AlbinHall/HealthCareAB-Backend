ac8b1f1ecf7ad1e95bf0a2f3a1ae8ddb1afc8bf1
Axel Calås <axel.calas.semst@gmail.com>
2025-01-15 09:41:37 +0100

    Added new properties to the User class.

--- HealthCareABApi/Models/User.cs
+++ HealthCareABApi/Models/User.cs
@@ -8,7 +8,9 @@ namespace HealthCareABApi.Models
         public int Id { get; set; }
         public required string Username { get; set; }
         public required string PasswordHash { get; set; }
-
+        public required string Firstname { get; set; }
+        public required string Lastname { get; set; }
+        public required string Email { get; set; }
         public virtual List<UserRole> Roles { get; set; } = [];
     }
 }
